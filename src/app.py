280102aa0ac673b1aebed63a679f87d1919236eb
Vlad <dosterm@wit.edu>
2019-03-26 18:04:46 -0500

    Cleaned up responses and overall ap structure

--- src/app.py
+++ src/app.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
-
 """
     File name: app.py
     Author: Vlad Doster
     Maintainer: Vlad Doster
     Date created: 3/18/2019
-    Date last modified: 3/18/2019
+    Date last modified: 3/26/2019
     Python Version: 3.6
 """
 
@@ -16,19 +15,14 @@ import os
 # 3rd Party libs
 from dotenv import load_dotenv
 from flask import Flask, request, redirect, session, url_for, flash, render_template
+from oauthlib.oauth2 import MissingTokenError
 from requests_oauthlib import OAuth2Session
-
 
 # Set logging to debug
 logging.basicConfig(level=logging.DEBUG)
 
 # Load env variables from .env
 load_dotenv()
-
-# Set HTTPS
-# context = SSL.Context(SSL.SSLv23_METHOD)
-# context.use_privatekey_file("key.pem")
-# context.use_certificate_file("cert.pem")
 
 # Instantiate app
 app = Flask(__name__)
@@ -45,16 +39,14 @@ redirect_uri = 'https://127.0.0.1:5000/callback'
 
 @app.route("/", methods=["GET"])
 def index():
+    """Index page"""
     return render_template('index.html')
 
 
 @app.route("/login", methods=["GET"])
-def demo():
-    """Step 1: User Authorization.
-
-    Redirect the user/resource owner to the OAuth provider (i.e. Github)
-    using an URL with a few key OAuth parameters.
-    """
+def initiate_oauth2_authorization():
+    """When sending the user to HubSpot's OAuth 2.0 server, first step is to create the authorization URL. This will
+    identify your app, and define the resources that it's requesting access to on behalf of the user. """
     hubspot = OAuth2Session(client_id, scope=scope, redirect_uri=redirect_uri)
     authorization_url, state = hubspot.authorization_url(authorization_base_url)
 
@@ -66,12 +58,10 @@ def demo():
 # Step 2: User authorization, this happens on the provider.
 @app.route("/callback", methods=["GET"])
 def callback():
-    """ Step 3: Retrieving an access token.
-
-    The user has been redirected back from the provider to your registered
-    callback URL. With this redirection comes an authorization code included
-    in the redirect URL. We will use that to obtain an access token.
-    """
+    """When the user has completed the consent prompt from step 3, the OAuth 2.0 server sends a GET request to the
+    redirect URI specified in your authentication URL. If there are no issues and the user approves the access
+    request, the request to the redirect URI will have a code query parameter attached when it's returned. If the
+    user doesn't grant access, no request will be sent."""
 
     code = request.values.get('code')
     hubspot = OAuth2Session(client_id, state=session['oauth_state'])
@@ -89,27 +79,30 @@ def callback():
 
 @app.route("/refresh_token", methods=['GET'])
 def refresh_token():
-    # Check if we have a token to refresh
-    if session.get('oauth_token'):
+    """Use a previously obtained refresh token to generate a new access token.  Access tokens expire after 6 hours,
+    so if you need offline access to data in HubSpot, you'll need to store the refresh token you get when initiating
+    your OAuth integration, and use that to generate a new access token once the initial access token expires. """
+    logging.info(session['oauth_token'].get("refresh_token"))
+    try:
         hubspot = OAuth2Session(client_id, state=session['oauth_state'])
-        # Request a new token
         new_token = hubspot.refresh_token(token_url,
-                                          body="grant_type=refresh_token&client_id=" + client_id + "&client_secret=" + client_secret + "&refresh_token=" +
-                                               session['oauth_token'].get('refresh_token'))
-        # Set new token
+                                          body=f"""grant_type=refresh_token&client_id={client_id}&client_secret={client_secret}&refresh_token={
+                                          session['oauth_token'].get('refresh_token')}""")
         session['oauth_token'] = new_token
         # Convey success to user
         flash("Token refreshed successfully!")
         return redirect(url_for('.index'))
-    # If no token present, we redirect to index
-    else:
+    except MissingTokenError as e:
         flash("Try logging in to access this resource!")
-        return redirect(url_for(".index"))
-
+        return redirect(url_for('.index'))
+    except Exception as e:
+        logging.error(f"Error refreshing token! {e}")
+        return redirect(url_for('.index'))
 
 @app.route("/token_info", methods=['GET'])
 def get_token_info():
-    # Check to see if there is a token to derive info from
+    """This can be used to get the email address of the HubSpot user that the token was created for, as well as the
+    Hub ID that the token is associated with. """
     if session.get("oauth_token"):
         token_info_url = f'https://api.hubapi.com/oauth/v1/refresh-tokens/{session["oauth_token"].get("refresh_token")}'
         hubspot = OAuth2Session(client_id, state=session['oauth_state'])
@@ -121,13 +114,23 @@ def get_token_info():
         return redirect(url_for(".index"))
 
 
-#
-# @app.route("/contacts", methods=["GET"])
-# def contacts():
-#     """Fetching a protected resource using an OAuth 2 token.
-#     """
-#     hubspot = OAuth2Session(client_id, token=session['oauth_token'])
-#     return jsonify(hubspot.get('https://api.hubapi.com/contacts/v1/lists/all/contacts/all').json())
+@app.route("/delete_refresh_token", methods=['GET'])
+def delete_refresh_token():
+    """Deletes a refresh token. You can use this to delete your refresh token if a user uninstalls your app."""
+    if session.get("oauth_token"):
+        token_info_url = f'https://api.hubapi.com/oauth/v1/refresh-tokens/{session["oauth_token"].get("refresh_token")}'
+        hubspot = OAuth2Session(client_id, state=session['oauth_state'])
+        data = hubspot.delete(token_info_url)
+        if data.status_code == 204:
+            flash("Refresh token successfully deleted!")
+            session['oauth_token'] = None
+            session['oauth_state'] = None
+            return redirect(url_for(".index"))
+    # If no token present, we redirect to index
+    else:
+        flash("Try logging in to access this resource!")
+        return redirect(url_for(".index"))
+
 
 if __name__ == "__main__":
     session.init_app(app)
